c8f35428824cd285fd4d19342bc7279656cf1082
Jomari Lagumen <162972412+GaddoStaffu@users.noreply.github.com>
2026-03-20 22:32:26 +0800

    remove hard coded img src

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const totalImages = 20;
 for (let i = 1; i <= totalImages; i++) {
   const img = document.createElement("img");
   img.src = `public/Artwork/artwork${i}.png`;
-  img.alt = "Artwork";
+  img.alt = `Artwork ${i}`;
   img.onerror = () => img.remove();
   gallery.appendChild(img);
 }
